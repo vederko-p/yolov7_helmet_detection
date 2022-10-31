35cdffdafd2ac59b1c07b5dce295c1c3c8a5a819
Skibin Maksim <ski6a_23@mail.ru>
2022-10-31 20:17:48 +0300

    build project script: logging echo added

--- build_project.sh
+++ build_project.sh
@@ -1,4 +1,5 @@
 # get yolo repository:
+echo 'Downloading yolov7 repository and install requirements ...'
 cd src/models
 git clone https://github.com/WongKinYiu/yolov7.git && cd yolov7
 pip install -r requirements.txt
