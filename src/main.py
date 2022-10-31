ae0f26482521dc9967fbfb1a32e33a956b7a066a
Skibin Maksim <ski6a_23@mail.ru>
2022-10-31 21:17:07 +0300

    minor changes

--- src/main.py
+++ src/main.py
@@ -6,4 +6,8 @@ from common.dataset_config import DATASETS_METADATA_PATH
 if __name__ == '__main__':
     with open(DATASETS_METADATA_PATH, 'r') as yaml_file:
         ds_meta = yaml.load(yaml_file, Loader=yaml.Loader)
-    print(ds_meta)
+    
+    # print(ds_meta)
+
+    print(ds_meta['safety_vest'])
+    
