f8468a7c8be056561b4146b45c2b3cb44173a1c1
Skibin Maksim <ski6a_23@mail.ru>
2022-11-01 18:35:27 +0300

    yaml utilities: open function name refactor + dump function added

--- src/utils/yaml_utils.py
+++ src/utils/yaml_utils.py
@@ -1,8 +1,15 @@
 from yaml import load, Loader
+from yaml import dump
 
 
-def open_yaml(filepath: str) -> dict:
+def read_yaml(filepath: str) -> dict:
     """Open yaml file."""
     with open(filepath, 'r') as yaml_file:
         data = load(yaml_file, Loader=Loader)
     return data
+
+
+def save_yaml(filepath: str, data) -> None:
+    """Save data into yaml file"""
+    with open(filepath, 'w') as yaml_file:
+        output = dump(data, yaml_file)
