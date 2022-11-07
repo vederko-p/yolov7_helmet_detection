dfdf7c556e4deb02608212221f751805f04caeca
Skibin Maksim <ski6a_23@mail.ru>
2022-11-07 15:01:00 +0300

    yaml utils: add regular files utils

--- src/utils/yaml_utils.py
+++ src/utils/yaml_utils.py
@@ -13,3 +13,21 @@ def save_yaml(filepath: str, data) -> None:
     """Save data into yaml file"""
     with open(filepath, 'w') as yaml_file:
         output = dump(data, yaml_file)
+
+
+def read_file_lines(filepath: str) -> List[str]:
+    """Read lines from file withouh new line symbols."""
+    with open(filepath, 'r') as file:
+        lines = list(map(lambda s: s[:-1], file.readlines()))
+    return lines
+
+
+def save_lines_into_file(
+    filepath: str,
+    lines: List[str],
+    read_mode: str = 'w'
+) -> None:
+    """Save lines of strings withouh new line symbols into file."""
+    lines_eol = map(lambda line: f'{line}\n', lines)
+    with open(filepath, read_mode) as file:
+        file.writelines(lines_eol)
