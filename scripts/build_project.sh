a63e1d87574333b2dcea29b77831dc8fad8d688b
Skibin Maksim <ski6a_23@mail.ru>
2022-11-09 16:38:26 +0300

    build project script: check models folder

--- scripts/build_project.sh
+++ scripts/build_project.sh
@@ -1,5 +1,12 @@
 # get yolo repository:
 echo 'Downloading yolov7 repository and install requirements ...'
-cd src/models
+
+MODELS_DIR="../src/models"
+if [[ ! -d $MODELS_DIR ]]
+then
+	mkdir $MODELS_DIR
+fi
+
+cd $MODELS_DIR
 git clone https://github.com/WongKinYiu/yolov7.git && cd yolov7
 pip install -r requirements.txt
