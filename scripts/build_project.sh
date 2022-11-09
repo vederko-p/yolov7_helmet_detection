f27fc401a767fa64d5037e6ddaf4713b8cc8cd37
Skibin Maksim <ski6a_23@mail.ru>
2022-11-09 22:47:04 +0300

    scripts: build project: install requirements

--- scripts/build_project.sh
+++ scripts/build_project.sh
@@ -1,12 +1,18 @@
-# get yolo repository:
-echo 'Downloading yolov7 repository and install requirements ...'
+# Install project requirements
+echo Install project requirements...
+pip -r requirements.txt
+
+# Get yolo repository
+echo 'Download yolov7 repository...'
 
 MODELS_DIR="../src/models"
 if [[ ! -d $MODELS_DIR ]]
 then
-	mkdir $MODELS_DIR
+  mkdir $MODELS_DIR
 fi
 
 cd $MODELS_DIR
 git clone https://github.com/WongKinYiu/yolov7.git && cd yolov7
+
+echo Install yolov7 requirements...
 pip install -r requirements.txt
